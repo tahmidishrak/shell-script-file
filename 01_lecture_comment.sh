92cc313c2d4490be82906d499e0281d45bef480c
Tahmid Ishrak <tanimtahmidishrak@gmail.com>
2026-09-22 23:34:23 +0600

    Start Shell Scripting Lecture

---
+++ 01_lecture_comment.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Comments in Shell Scripting
+# this is a sigle line comment
+echo "# This is a sigle line comment"
+echo "---------------------------"
+
+:<< comment
+this is a multi line comment
+comment
+
+echo ":<< comment
+this is a multi line comment
+comment"
---
+++ 02_lecture_variable.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Defining Varibale
+
+# name="tahmid"
+# age=15
+# echo "my name is $name and my age is $age"
+
+
+# hostName=$(hostname)
+# PWD=$(pwd)
+# echo "Host name is $hostName and root is $PWD"
+
+
+# Constent variable
+
+# readonly name="tahmid"
+# echo "My name is $name"
+# name="tanim"
+# echo "My name is $name"
